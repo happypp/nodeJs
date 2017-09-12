8312c706d28a50b7c4c2ff1ed5ea8475a1a1ab64
pp <957655440@qq.com>
2017-09-12 21:59:51 +0800

    NODEJS 正则实现动态页面
    Signed-off-by: pp <957655440@qq.com>

--- routAndExc/router.js
+++ routAndExc/router.js
@@ -4,10 +4,19 @@
 let optfile = require('./optfile');
 let url = require("url");
 var  queryString = require('querystring');  //post需导入
-getRecall = (req,res) => {
+getRecall = (req,res,post) => {
   res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
   recall = (data) => {
-    res.write(data);
+    // res.write(data);
+    //实现将参数动态绑定到HTML页面上
+    let dataStr = data.toString();
+    let arr = ['email','pwd'];
+    for (let i = 0; i < arr.length; i++){
+      let re = new RegExp('{' + arr[i] + '}','g');
+      console.log(post[arr[i]]);
+      dataStr = dataStr.replace(re,post[arr[i]]);
+    }
+    res.write(dataStr);
     res.end('');//不写则没有http协议尾
   };
   return  recall;
@@ -30,11 +39,10 @@ module.exports = {
       post = queryString.parse(post);
       console.log("email:" + post['email']);
       console.log("pwd:" + post['pwd']);
+
+      recall = getRecall(req,res,post);
+      optfile.readFile("./login.html",recall)
     });
-
-
-    recall = getRecall(req,res);
-    optfile.readFile("./login.html",recall)
   },
   regist : function (req,res) {
 
